4ebad4122f8d16e7e136b5e0048812010f0d74c6
Anurag Kumar <pumbatimon473@gmail.com>
2024-11-01 16:57:43 +0530

    Implemented 'Soft Delete' for the employees' records

--- employee_mgmt/models.py
+++ employee_mgmt/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+class NonDeleted(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    objects = NonDeleted()
 
     class Meta:
         abstract = True
--- employee_mgmt/viewsets.py
+++ employee_mgmt/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.request import Request
 from employee_mgmt.models import Employee
 from employee_mgmt.serializers import EmployeeSerializer
 
@@ -7,3 +8,7 @@ class EmployeeViewSet(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = 'id'
+
+    def perform_destroy(self, instance: Employee):
+        instance.is_deleted = True
+        instance.save()

12f7b39bc6bb35eb9c603d28d7494548958bf409
Anurag Kumar <pumbatimon473@gmail.com>
2024-11-04 05:27:36 +0530

    Added below models to the admin portal (Headless CMS): - employee_mgmt.Employee - hr_portal.User

--- hr_portal/admin.py
+++ hr_portal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    fields = ('username', 'password')
+    list_display = ('id', 'username', 'email')
+    search_fields = ('username',)
+
+admin.site.register(User, UserAdmin)
